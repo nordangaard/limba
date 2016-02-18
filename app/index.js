6e0fe4147084895f630d07c10c56b40c0f574017
Markus Nordangård <nordangaard@gmail.com>
2016-02-18 16:55:56 +0100

    Redux experiment

--- app/index.js
+++ app/index.js
@@ -1,10 +1,53 @@
-var riot = require('riot');
-var jquery = require('jquery');
-require('./tags/word.tag');
+const riot = require('riot')
+const jquery = require('jquery')
+const redux = require('redux')
+
+require('./tags/word.tag')
+
+let initState = {
+  words: [
+    {
+      word: 'Masina',
+      translation: 'Car',
+      gender: 'feminine',
+      number: 'singular'
+    }
+  ]
+}
+
+function counter(state = 0, action) {
+  switch (action.type) {
+  case 'INITIALIZE':
+    return initState;
+  case 'CHANGE_WORD':
+    state.words[0].word = action.data.word;
+    return state;
+  case 'DECREMENT':
+    return state - 1
+  default:
+    return state
+  }
+}
+
+let store = redux.createStore(counter)
+store.dispatch({type: 'INITIALIZE'});
+
+// You can subscribe to the updates manually, or use bindings to your view layer.
+store.subscribe(() =>
+  console.log(store.getState())
+)
+
+window.dis = function dis(word) {
+  store.dispatch({type: 'CHANGE_WORD', data: { word: word }})
+}
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  riot.mount('word', 'word', { '$': jquery });
+  riot.mount('word', 'word', { '$': jquery, data: store.getState().words[0] })
+  store.subscribe(() =>
+    riot.update()
+  )
 
 
-});
+
+})
--- app/tags/word.tag
+++ app/tags/word.tag
@@ -1,20 +1,14 @@
 <word class="word">
-  <h2>{this.data.word}</h2>
+  <h2>{this.opts.data.word}</h2>
 
   <div class="info">
-    <span>{this.data.translation}</span>
-    <span>{this.data.number}</span>
-    <span>{this.data.gender}</span>
+    <span>{this.opts.data.translation}</span>
+    <span>{this.opts.data.number}</span>
+    <span>{this.opts.data.gender}</span>
   </div>
 
   <script>
     var elem = this.opts.$(this.root);
-    this.data = {
-      word: 'Masina',
-      translation: 'Car',
-      gender: 'feminine',
-      number: 'singular'
-    };
     console.log('Hej');
     //elem.addClass('module');
   </script>
